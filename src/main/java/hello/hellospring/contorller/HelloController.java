6e69ae0de6e4af29b1e5e6ff6f8d991363144f28
ducndgus <40850293+ducndgus@users.noreply.github.com>
2022-02-19 22:57:24 +0900

    20220123 스프링 기본편

--- src/main/java/hello/hellospring/contorller/HelloController.java
+++ src/main/java/hello/hellospring/contorller/HelloController.java
@@ -2,6 +2,8 @@ package hello.hellospring.contorller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,36 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data",  "hello!!");//키,데이터
         return "hello";//hello.html을 찾아서 간다
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name; //hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+        public  String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name=name;
+        }
     }
 }
